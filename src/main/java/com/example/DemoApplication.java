478fa20a828fc73d9204bc7ba80bc003fa7d27de
shams <shams@viratech.ir>
2017-01-14 20:01:53 +0330

    spring security added

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,9 +2,7 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
