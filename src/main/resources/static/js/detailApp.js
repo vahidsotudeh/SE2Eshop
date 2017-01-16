3eb66278f5d972d03ebec9cc989f0f95cb3f6de5
vahidsotudeh@gmail.com <vahidsotudeh@gmail.com>
2017-01-17 00:11:43 +0330

    angular added to checkOutPage !!

--- src/main/resources/static/js/detailApp.js
+++ src/main/resources/static/js/detailApp.js
@@ -3,17 +3,14 @@ var baseUrl="http://172.20.185.149:8080/api";
 var imageBaseUrl="http://172.20.185.149:8080"; 
 var absoluteUrlfake="file:///C:/Users/Microsoft/IdeaProjects/SE2Eshop/src/main/resources/static/bookDetail.html?";
 var absoluteUrlOrigion="http://172.20.185.149:8080/bookDetail.html?";
-var absoluteUrl=absoluteUrlOrigion;
+var absoluteUrl=absoluteUrlfake;
 
 bookStoreApp.controller('bookDetailController',function bookDetailController($scope,$http,$cookies,$location,$routeParams) {
             $scope.currUrl=$location.absUrl().replace(absoluteUrl,"").replace("#!/","");
             $scope.bookId=$scope.currUrl.replace("bookId=","");
             $scope.shoppingCartItemsCount=$cookies.get("shoppingCartItemsCount")|0;
             $scope.numbers=[1,2,3,4,5];
-            // if($cookies.get("shoppingCartItemsCount")){
-            //     $scope.shoppingCartItemsCount=$cookies.get("shoppingCartItemsCount");
-            // }
-         $scope.fetchBookDetail=function () {  $http.get(baseUrl+"/books/"+$scope.bookId).then(function (resp) { 
+         $scope.fetchBookDetail=function () {  $http.get(baseUrl+"/books/detail/"+$scope.bookId).then(function (resp) { 
                 $scope.book=resp.data;
                 $scope.book.imageAddress=imageBaseUrl+$scope.book.imageAddress;
 
@@ -21,14 +18,23 @@ bookStoreApp.controller('bookDetailController',function bookDetailController($sc
              alert("error");
           });
         }
-        // alert($cookies.get("shoppingCartItemsCount"));
         $scope.fetchBookDetail();
         $scope.addToShoppingCart=function () {
-            $scope.shoppingCartItemsCount+=$scope.selectedCount;
-            $cookies.put("shoppingCartItemsCount",$scope.shoppingCartItemsCount);
-            $scope.individualItems=$cookies.get("individualItems")|1;
-            $cookies.put("individualItems",$scope.individualItems);
-            $cookies.put("book:"+bookId,$scope.selectedCount);
+            if($scope.selectedCount){
+                $scope.shoppingCartItemsCount+=$scope.selectedCount;
+                $cookies.put("shoppingCartItemsCount",$scope.shoppingCartItemsCount);
+                if(!$cookies.get("book:"+$scope.bookId)){
+                    $scope.individualItems=$cookies.get("individualItems")+1|1;
+                    var list=$cookies.get("bookList");
+                    if(list)
+                        $cookies.put("bookList",list+"-"+$scope.bookId);
+                    else{
+                        $cookies.put("bookList",$scope.bookId);
+                    }
+                }
+                $cookies.put("individualItems",$scope.individualItems);
+                $cookies.put("book:"+$scope.bookId,$scope.selectedCount);
+            }
           }
-        // $cookieStore.put('shoppingCartItemsCount','1');
+        
 });
